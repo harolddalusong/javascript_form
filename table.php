1082284368eebab25a6a322f4b8a659a63acd90b
COMLAB <hdalusong23-0303@cca.edu.ph>
2025-02-19 18:40:10 +0800

    dasd
    dads

---
+++ table.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Table Form</title>
+</head>
+<body>
+    <h1>Student Record</h1>
+    <table id="student_record">
+        <!-- WALANG LAMAN-->
+    </table>
+
+<body>
+</html>    
