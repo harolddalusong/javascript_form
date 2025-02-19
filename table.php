40e96b5ba83870a7225d441975546271c709f198
COMLAB <hdalusong23-0303@cca.edu.ph>
2025-02-19 19:52:46 +0800

    kusshhh
    tapos na

--- table.php
+++ table.php
@@ -1,12 +1,47 @@
 <html>
     <head>
-        <title>Table Form</title>
-</head>
-<body>
-    <h1>Student Record</h1>
-    <table id="student_record">
-        <!-- WALANG LAMAN-->
-    </table>
+        <title>Table form </title> 
+    </head>
+    <body>
+        <h1>Student Record</h1>
+        <table id="student_record" border="1";>  
+        </table>
+         <script>
+           const studentRecord =document.getElementById("student_record");
+           console.log("studentRecord");
 
-<body>
-</html>    
+           const row1 = studentRecord.insertRow();
+           const row1col1 =row1.insertCell();
+            row1col1.innerHTML ="Last Name";
+            const row1col2 =row1.insertCell();
+            row1col2.innerHTML ="First Name";
+
+            const row2 = studentRecord.insertRow();
+            const row2col1 =row2.insertCell();
+            const row2col2 =row2.insertCell();
+
+            const inputLastName = document.createElement("INPUT");
+            inputLastName.type="text";
+            inputLastName.placeholder= "Enter Last name";
+            row2col1.append (inputLastName);
+
+            const inputFirstName = document.createElement("INPUT");
+            inputFirstName.type="text";
+            inputFirstName.placeholder= "Enter First name";
+            row2col2.append (inputFirstName);
+
+            const row3 = studentRecord.insertRow();
+            const row3col1 = row3.insertCell();
+            row3col1.colSpan = 2;
+
+            const submitButton = document.createElement("button");
+            submitButton.type = "button";
+            submitButton.innerHTML = "Submit";
+            row3col1.append(submitButton);
+
+           console.log(studentRecord);
+
+         </script>
+    </body>
+</html>
+
